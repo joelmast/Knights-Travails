4f61d4ff26da7657aaeabdc7e5fec8fc8f95d605
Hamburger-coder <joesmast@gmail.com>
2025-07-31 18:13:48 -0400

    Finish project!

--- script.js
+++ script.js
@@ -7,64 +7,63 @@ const knightOffset = [
 
 function knightMoves(start, end) {
     let queue = [];
+    visitedPositions = []; // Reset for each call
 
-    queue.push({
+    let visitedCoords = new Set(); 
+
+    // Start node object
+    let startNode = {
         x: start[0],
         y: start[1],
         distance: 0,
-        predecessor: { x: null, y: null }
-    })
+        predecessor: null // Initial node has no predecessor
+    };
+
+    queue.push(startNode);
+    visitedCoords.add(`${startNode.x},${startNode.y}`); // Mark start node as visited
+
     while (queue.length > 0) {
         let current = queue.shift();
+
         if (current.x === end[0] && current.y === end[1]) {
             return getPath(current);
         }
-        visitedPositions.push(current);
-        let currentMoves = getPotentialMoves(current);
-        if (currentMoves > 0) {
-            for (let i = 0; i < currentMoves.length; i++) {
-                let currMove = currentMoves[i];
-                queue.push(
-                    {
-                        x: currMove.x,
-                        y: currMove.y,
-                        distance: currMove + 1,
-                        predecessor: { x: currMove.predecessor.x, y: currMove.predecessor.y }
-                    }                       
-                );
-            }
-        }
         
+        let potentialMoves = getPotentialMoves(current, end);
+
+        for (let i = 0; i < potentialMoves.length; i++) {
+            let next = potentialMoves[i];
+            const coordKey = `${next.x},${next.y}`;
+
+        if (!visitedCoords.has(coordKey)) {
+            visitedCoords.add(coordKey);
+            queue.push({
+                x: next.x,
+                y: next.y,
+                distance: next.distance,
+                predecessor: current
+            });
+        }
+        }
     }
+    return null; // No path found
 }
 
-function hasNotBeenVisited(obj) {
-    let isOnList = visitedPositions.some(visitedSquare => {
-        return visitedSquare.x === obj.x && visitedSquare.y === obj.y;
-    })
-    return !isOnList;
-}
-
+// hasNotBeenVisited is no longer needed with visitedCoords Set
 
 // return in correct form (object)
 function getPotentialMoves(position) {
-    // get all legal moves for the knight 
     let legalMoves = [];
     for (let i = 0; i < knightOffset.length; i++) {
         let currOffset = knightOffset[i];
         let newX = position.x + currOffset[0];
         let newY = position.y + currOffset[1];
-        if (isLegal({
-                x: newX, 
-                y: newY,
-                distance: position.distance + 1,
-                predecessor: { x: position.x, y: position.y }
-            })) {
+        
+        if (isLegal({x: newX, y: newY})) {
             legalMoves.push({
                 x: newX, 
                 y: newY,
-                distance: position.distance + 1,
-                predecessor: { x: position.x, y: position.y }
+                distance: position.distance + 1
             });
         }
     }
@@ -72,18 +71,19 @@ function getPotentialMoves(position) {
 }
 
 function isLegal(obj) {
-    if ((obj.x >= 0 && obj.x <= 7) && (obj.y >= 0 && obj.y <= 7)) {
-        return true;
-    } else {
-        return false;
-    }
+    // A quick way to make sure it's within the 8x8 board (0 to 7)
+    return (obj.x >= 0 && obj.x <= 7) && (obj.y >= 0 && obj.y <= 7);
 }
 
 function getPath(obj) {
-    let path = []
-    while (obj.predecessor.x !== null) {
-        path.push([obj.predecessor.x, obj.predecessor.y])
+    let path = [];
+    let current = obj;
+  
+    while (current !== null) {
+        path.push([current.x, current.y]);
+        current = current.predecessor;
     }
-    return path;
+    return path.reverse();
 }
 
+console.log(knightMoves([0,0],[7,7]));
